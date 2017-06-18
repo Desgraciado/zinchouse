137c7ad21b88601ca6a89d4fa00c64aa665217f5
David Menne <davidmenne@maliweb.at>
2017-06-18 08:56:47 +0200

    change something to test jenkins

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var app = express();
 
+// Testing the jenkins server
 app.get('/',function(req,res) {
-	  res.send("Hello World");
+	  res.send("Hello World!");
 });
 
 app.listen(4000);
